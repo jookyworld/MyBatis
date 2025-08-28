607c94836885546f1823cfd8a2b70eb6534a2978
jooky <jooky0327@naver.com>
2025-08-28 14:36:19 +0900

    xml로 분리 성공

--- src/main/java/com/mybatis/domain/member/member/repository/MemberRepository.java
+++ src/main/java/com/mybatis/domain/member/member/repository/MemberRepository.java
@@ -2,15 +2,14 @@ package com.mybatis.domain.member.member.repository;
 
 import com.mybatis.domain.member.member.dto.Member;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface MemberRepository {
 
-    @Select("""
-            SELECT * FROM member
-            """)
+//    @Select("""
+//            SELECT * FROM member
+//            """)
     List<Member> findAll();
 }
